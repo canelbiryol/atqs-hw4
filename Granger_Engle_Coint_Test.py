9606dd211f31c96ac06578ae953e5331cb045908
Canel Biryol <canelbiryol@gmail.com>
2018-05-03 22:50:01 -0400

    Question 1 completed

--- Granger_Engle_Coint_Test.py
+++ Granger_Engle_Coint_Test.py
@@ -5,89 +5,66 @@ Created on Apr 28, 2018
 '''
 import numpy as np
 import pandas as pd
-import statsmodels.tsa.stattools as tsa
 
-from numpy.linalg import LinAlgError
-from scipy import stats
+from statsmodels.tsa.stattools import adfuller
+from  itertools import combinations
 
-from statsmodels.regression.linear_model import OLS, yule_walker
-from statsmodels.tools.tools import add_constant, Bunch
-from statsmodels.tsa.tsatools import lagmat, lagmat2ds, add_trend
-from statsmodels.tsa.adfvalues import mackinnonp, mackinnoncrit
-from statsmodels.tsa._bds import bds
-from statsmodels.tsa.arima_model import ARMA
-from statsmodels.tools.sm_exceptions import InterpolationWarning, MissingDataError
-
-SQRTEPS = np.sqrt(np.finfo(np.double).eps)
+sig_level = '1%' #significance level for the ADF Test
+window_size = 40
 
 # Loading data
 data_path = './CointData.csv'
 
-def linreg(x, y):
-    # y=x*alpha+beta
-    window_size = len(x)
-    
-    x2=np.power(x,2)
-    xy=x*y
-    window = np.ones(int(window_size))
-    a1=np.convolve(xy, window, 'full')*window_size
-    a2=np.convolve(x, window, 'full')*np.convolve(y, window, 'full')
-    b1=np.convolve(x2, window, 'full')*window_size
-    b2=np.power(np.convolve(x, window, 'full'),2)
-    alphas=(a1-a2)/(b1-b2)
-    betas=(np.convolve(y, window, 'full')-alphas*np.convolve(x, window, 'full'))/float(window_size)
-    alphas=alphas[:-1*(window_size-1)] #numpy array of rolled alpha
-    betas=betas[:-1*(window_size-1)]   #numpy array of rolled beta
-    return alphas
+def unitRootTest(df):
+    for col in df.columns.values:
+        result = adfuller(df[col])
+        ADF_stat = result[0]
+        if ADF_stat < result[4].get(sig_level): #reject the null, this means no unit root I(0) for this significance level, data is stationary
+            print('No Unit Root, stationary')
+            print(col)    
+        else:
+            df = df.drop(columns=[col]) # drop the I(1) non-stationary time series
+    return df
 
-def coint(y0, y1, trend='c', method='aeg', maxlag=None, autolag='aic',
-          return_results=None):
-    
-
-    trend = trend.lower()
-    if trend not in ['c', 'nc', 'ct', 'ctt']:
-        raise ValueError("trend option %s not understood" % trend)
-    y0 = np.asarray(y0)
-    y1 = np.asarray(y1)
-    if y1.ndim < 2:
-        y1 = y1[:, None]
-    nobs, k_vars = y1.shape
-    k_vars += 1   # add 1 for y0
-
-    if trend == 'nc':
-        xx = y1
-    else:
-        xx = add_trend(y1, trend=trend, prepend=False)
-
-    res_co = OLS(y0, xx).fit()
-
-    if res_co.rsquared < 1 - 100 * SQRTEPS:
-        res_adf = tsa.adfuller(res_co.resid, maxlag=maxlag, autolag=autolag,
-                           regression='nc')
-    else:
-        import warnings
-        warnings.warn("y0 and y1 are (almost) perfectly colinear."
-                      "Cointegration test is not reliable in this case.")
-        # Edge case where series are too similar
-        res_adf = (-np.inf,)
-
-    # no constant or trend, see egranger in Stata and MacKinnon
-    if trend == 'nc':
-        crit = [np.nan] * 3  # 2010 critical values not available
-    else:
-        crit = mackinnoncrit(N=k_vars, regression=trend, nobs=nobs - 1)
-        #  nobs - 1, the -1 is to match egranger in Stata, I don't know why.
-        #  TODO: check nobs or df = nobs - k
-
-    pval_asy = mackinnonp(res_adf[0], regression=trend, N=k_vars)
-    return res_adf[0], pval_asy, crit
-
+def calculateGamma(x, y, window_size):
+    df_coint = pd.DataFrame()
+    df_coint['x'] = x 
+    df_coint['y'] = y
+    df_coint['xy'] = df_coint['x'] * df_coint['y']
+    df_coint['x^2'] = df_coint['x']**2
+    df_coint['x_t-1'] = df_coint['x'].shift(1)
+    df_coint['y_t-1'] = df_coint['y'].shift(1)
+    df_coint['m'] = ((pd.rolling_mean(df_coint['x'], window_size) * pd.rolling_mean(df_coint['y'], window_size)) 
+    - (pd.rolling_mean(df_coint['xy'], window_size) )) / (pd.rolling_mean(df_coint['x^2'], window_size) - pd.rolling_mean(df_coint['x'], window_size)**2 )
+    df_coint['b'] = pd.rolling_mean(df_coint['y'], window_size) - df_coint['m'] * pd.rolling_mean(df_coint['x'], window_size)
+    df_coint['gamma_numerator'] = (df_coint['y'] - (df_coint['m'] * df_coint['x'] + df_coint['b'])) * (df_coint['y_t-1'] - (df_coint['m'] * df_coint['x_t-1'] + df_coint['b']))
+    df_coint['gamma_denominator'] = ((df_coint['y_t-1'] - (df_coint['m'] * df_coint['x_t-1'] + df_coint['b'])))**2
+    df_coint['gamma'] = (df_coint['gamma_numerator'].rolling(window_size).sum()) / (df_coint['gamma_denominator'].rolling(window_size).sum())
+    return df_coint
     
 if __name__ == '__main__':
     df = pd.read_csv(data_path, header=None)
+    #print(len(df.columns))
+
+    df = unitRootTest(df)
+             
     #print(df.head())
-    print(linreg(df[0], df[1]))
-    print(tsa.adfuller(df[0]))
+    
+    pairs = list(combinations(df.columns,2))
+    
+
+    for pair in pairs:
+        print('{}-{}'.format(pair[0], pair[1]))
+        x = df[pair[0]]
+        y = df[pair[1]]
+        df_coint = calculateGamma(x, y, window_size)
+         
+        df_coint.to_csv('./results/{}_{}.csv'.format(pair[0], pair[1]))
+     
+    # print(df_coint)
+   
+    
+
     
 #     for col in df.columns.values:  #or edit this for a subset of columns first
 #         adf_results[col] = tsa.adfuller(df[col])
